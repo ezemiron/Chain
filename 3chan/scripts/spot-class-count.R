00a6fa988ff5ab2313ed3c754426da8a96f64547
ezequiel.mironsardiello@bioch.ox.ac.uk <wolf4192@bioch2125.bioch.ox.ac.uk>
2018-10-16 19:12:58 +0100

    restored license to spot-class-count

--- 3chan/scripts/spot-class-count.R
+++ 3chan/scripts/spot-class-count.R
@@ -1,4 +1,27 @@
-dirchosen <- "/home/roel/Documents/papers/chrom_marks/results"
+#!/usr/bin/env Rscript
+##
+## Copyright (C) 2016 Ezequiel Miron <eze.miron@bioch.ox.ac.uk>
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+dirchosen <- readline(prompt = "This script should be run two directories above the marks")
+
+dirchosen <- getwd()
+
+
 
 Cell = "C127"
 Condition = "MS"
